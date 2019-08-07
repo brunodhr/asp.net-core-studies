d9828a2567dcd6271073b79e36054b0901322500
Bruno <brunofiliperamos@hotmail.com>
2019-08-06 23:21:55 -0300

    feat: create todo

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -20,14 +20,14 @@ namespace dotnet_studies.Controllers
             _context.TodoItems.Add(new TodoItem { Name = "Item2" });
             _context.SaveChanges();
         }
-        // GET: api/todo
+        // GET: api/todo, index todo's
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
             return await _context.TodoItems.ToListAsync();
         }
 
-        // GET: api/todo/{id}
+        // GET: api/todo/{id}, show todo
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
         {
@@ -39,6 +39,16 @@ namespace dotnet_studies.Controllers
             }
 
             return todoItem;
+        }
+
+        // POST: api/todo, create todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
         }
     }
 }
