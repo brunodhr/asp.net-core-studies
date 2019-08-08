69330f7d46f4f13d41baa13e6e23f894d19060a6
Bruno <brunofiliperamos@hotmail.com>
2019-08-07 21:39:13 -0300

    feat: todo put for edit

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -50,5 +50,20 @@ namespace dotnet_studies.Controllers
 
             return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
         }
+
+        // PUT: api/todo/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
